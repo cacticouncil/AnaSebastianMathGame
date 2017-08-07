3b4d0a16fdff18382a4df12b5498a68ed3739fbb
Pablo Bernal Alarcón <pbernalalarcon@fullsail.edu>
2017-08-07 09:24:50 -0400

    3D movement started

--- Anti Math Remastered/Assets/Scripts/PlayerController.cs
+++ Anti Math Remastered/Assets/Scripts/PlayerController.cs
@@ -13,7 +13,9 @@ public class PlayerController : MonoBehaviour {
 
     Vector3 rotL = new Vector3(0, 0, 1);
     Vector3 rotR = new Vector3(0, 0, -1);
-    
+    Vector3 rotU = new Vector3(1, 0, 0);
+    Vector3 rotD = new Vector3(-1, 0, 0);
+
     Rigidbody rb;
 	// Use this for initialization
 	void Start () {
@@ -28,24 +30,32 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
         // daddy.transform.Rotate(0,0, -Input.gyro.rotationRateUnbiased.z);
-        float angleZ = -Input.acceleration.x;
-        if (phone && (angleZ > 0.1 || angleZ < -0.1))
+        float angleZ = Input.acceleration.z;
+        float angleY = Input.acceleration.y;
+        float angleX = Input.acceleration.x;
+        if (phone && (-angleX > 0.1 || -angleX < -0.1))
         {
-             Vector3 rot = new Vector3(0, 0,  angleZ);
-            transform.RotateAround(Vector3.zero, rot, Time.deltaTime * 100*Mathf.Abs(angleZ));
+            Vector3 rot = new Vector3(0, 0,  -angleX);
+            transform.RotateAround(Vector3.zero, rot, Time.deltaTime * 100*Mathf.Abs(-angleX));
         }
         else
         {
+
+           // Vector3 rot = new Vector3(0, 0, angleX);
+           // transform.RotateAround(Vector3.zero, rot, Time.deltaTime * 50 * Mathf.Abs(angleX));
             if (Input.GetKey(KeyCode.LeftArrow))
-                transform.RotateAround(Vector3.zero, rotL, Time.deltaTime * 100);
-
+                transform.Rotate(new Vector3(0,1,0), Time.deltaTime * 100);
+            
             if (Input.GetKey(KeyCode.RightArrow))
-                transform.RotateAround(Vector3.zero, rotR, Time.deltaTime * 100);
+                transform.Rotate(new Vector3(0, -1, 0), Time.deltaTime * 100);
+            if (Input.GetKey(KeyCode.UpArrow))
+                transform.RotateAround(Vector3.zero, rotU, Time.deltaTime * 100);
 
-            // if (Input.GetKeyDown(KeyCode.UpArrow))
-            //     rb.AddRelativeForce(0, 500, 0);
+            if (Input.GetKey(KeyCode.DownArrow))
+                transform.RotateAround(Vector3.zero, rotD, Time.deltaTime * 100);
+
         }
-        text.GetComponent<Text>().text = angleZ.ToString();
+        text.GetComponent<Text>().text = "X: " +angleX.ToString()+ " Y: " + angleY.ToString()+ " Z: " + angleZ.ToString();
 
     }
 }
